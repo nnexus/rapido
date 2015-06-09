ae21df9b1e2d32783108808fc044a13d94b06a39
nnexus <patrik.helminen@nnexus.net>
2015-06-09 23:06:57 +0300

    uusi metodeita muistipeli.java

--- rapido/src/main/java/Muistipeli.java
+++ rapido/src/main/java/Muistipeli.java
@@ -10,6 +10,39 @@
 import java.util.Scanner;
 public class Muistipeli {
 
+    public static void alustaPelitaulukko(char[][] taulukko)
+    {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+              taulukko[i][j] = ' ';
+            }
+        }
+    }
+    
+    public static void piirraPelitaulukko(char[][] taulukko)
+    {
+        System.out.println(" --------");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 4; j++) {
+                System.out.print(taulukko[i][j] + " ");
+            }
+            System.out.println("|");
+    }
+        System.out.println(" --------");
+        System.out.println();
+    }
+    
+    public static void arvoPelitaulukkoon(char[][] taulukko)
+    {
+        int x = (int) (Math.random() * 4);
+            int y = (int) (Math.random() * 4);
+                if (taulukko[y][x] == ' ')
+                {
+                    taulukko[y][x] = 'X';
+                   
+                }
+    }
     
     public static void valikko()
     {
@@ -20,15 +53,17 @@ public class Muistipeli {
              
         ----------------------------------------------------------------------*/         
         char valinta;
+               
         Scanner valikonlukija = new Scanner(System.in);
-        System.out.println("Valitse jokin vaihtoehto: (U)usi peli, (V)aikeusaste, top-10 (P)isteet, (O)hjeet, (L)opeta");  
+        System.out.println("Valitse jokin vaihtoehto: (U)usi peli, (V)aikeusaste, (P)isteet top-10, (O)hjeet, (L)opeta");  
         valinta = valikonlukija.findInLine(".").charAt(0); //luetaan char -tyyppinen syöte
-         
-            switch (valinta) {
+             
+           switch (valinta) {
             case 'U':
                 System.out.println("Uusi peli");
                 // aloittaa uuden pelin valitulla vaikeusteella, oletus 1
                 // GUI:ssa aloittaa uuden pelin valitulla vaikeusasteella, oletus 1
+          
                 break;
             case 'V':
                 System.out.println("Vaikeusaste");
@@ -44,24 +79,36 @@ public class Muistipeli {
                 System.out.println("Ohjeet");
                 // printataan lyhyet ohjeet miten peliä pelataan - .txt -filenä?
                 // GUI:ssa ohjeet pop uppina
-                break; 
+                break;
+                
+           
             case 'L':
-                System.out.println("Lopeta");
+                System.out.println("Lopetit!");
                 // lopettaa pelin ja palaa alkuvalikkoon
                 // GUI:ssa lopettaa pelin ja palaa alkuvalikkoon
                 break;
-            }     
+            }
+            System.out.println("TESTI ulos switch");
     }
    
   
     public static void main(String[] args) {
+        
+        char[][] pelitaulukko = new char[4][4];
+        alustaPelitaulukko(pelitaulukko);
+        piirraPelitaulukko(pelitaulukko);
+
+
     
         System.out.println("Tämä on muistipeli espanjankielisillä sanoilla!");
     
        
-     
-         valikko();
-        
+        while(true)
+        {
+            valikko();
+         
+       
+        }
      
         
      // MÄÄRITETÄÄN GRIDin koko   
